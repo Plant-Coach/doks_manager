435daab1af1201c9fe5da5b1730442b8edb38caa
Joel Grant <69736499+joel-grant@users.noreply.github.com>
2025-04-25 22:45:19 -0600

    Set up DigitalOcean K8s Cluster config (#3)
    * test pipeline
    
    * fix path
    
    * add env variable
    
    * update workspace name
    
    * try fixing workspace again
    
    * set up cluster and database
    
    * test pipeline fix
    
    * add terraform to gitignore
    
    * comment out actions

---
+++ backend.tf
@@ -0,0 +1,10 @@
+terraform { 
+  cloud { 
+    
+    organization = "plant-coach" 
+
+    workspaces { 
+      name = "do-k8s" 
+    } 
+  } 
+}
---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.51.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+}
+
+# Added straight from Terraform documentation.
+# https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/kubernetes_cluster#kubernetes-terraform-provider-example
+# data "digitalocean_kubernetes_cluster" "example" {
+#   name = "plant-coach"
+# }
+
+# provider "kubernetes" {
+#   host  = data.digitalocean_kubernetes_cluster.example.endpoint
+#   token = data.digitalocean_kubernetes_cluster.example.kube_config[0].token
+#   cluster_ca_certificate = base64decode(
+#     data.digitalocean_kubernetes_cluster.example.kube_config[0].cluster_ca_certificate
+#   )
+# }
