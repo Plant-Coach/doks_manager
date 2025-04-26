1b506f9ca341ebd16d0f899bb49a68775f5efc3e
Joel Grant <joelmgrant@me.com>
2025-04-25 22:40:12 -0600

    set up cluster and database

--- main.tf
+++ main.tf
@@ -2,11 +2,33 @@ resource "digitalocean_kubernetes_cluster" "plant_coach_be_cluster" {
   name   = "plant-coach"
   region = "nyc1"
   # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.22.8-do.1"
+  version = "1.32"
+
+  tags = [
+    "ruby",
+    "rails",
+    "plant-coach",
+    "dev"
+  ]
 
   node_pool {
     name       = "worker-pool"
     size       = "s-2vcpu-2gb"
     node_count = 1
   }
+}
+
+resource "digitalocean_database_postgresql_config" "plant_coach_be_cluster" {
+  cluster_id = digitalocean_database_cluster.plant_coach_be_cluster.id
+  timezone   = "UTC"
+  work_mem   = 16
+}
+
+resource "digitalocean_database_cluster" "plant_coach_be_cluster" {
+  name       = "plant-coach-be-cluster-postgresql-cluster"
+  engine     = "pg"
+  version    = "17"
+  size       = "db-s-1vcpu-1gb"
+  region     = "nyc1"
+  node_count = 1
 }
--- provider.tf
+++ provider.tf
@@ -8,5 +8,18 @@ terraform {
 }
 
 provider "digitalocean" {
-  token = "dop_v1_592de439fe92c612c88cedc7813b9c290710e45dec8f1975d41c9bc8061b4a08"
 }
+
+# Added straight from Terraform documentation.
+# https://registry.terraform.io/providers/digitalocean/digitalocean/latest/docs/resources/kubernetes_cluster#kubernetes-terraform-provider-example
+# data "digitalocean_kubernetes_cluster" "example" {
+#   name = "plant-coach"
+# }
+
+# provider "kubernetes" {
+#   host  = data.digitalocean_kubernetes_cluster.example.endpoint
+#   token = data.digitalocean_kubernetes_cluster.example.kube_config[0].token
+#   cluster_ca_certificate = base64decode(
+#     data.digitalocean_kubernetes_cluster.example.kube_config[0].cluster_ca_certificate
+#   )
+# }
