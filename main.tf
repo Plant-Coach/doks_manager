c6c9c9d81b6f35a5db195c000077e9ab244ebca4
Joel Grant <joelmgrant@me.com>
2025-05-22 09:54:06 -0600

    solidify k8s data base and cluster configurations

--- main.tf
+++ main.tf
@@ -1,34 +1,94 @@
-resource "digitalocean_kubernetes_cluster" "plant_coach_be_cluster" {
-  name   = "plant-coach"
-  region = "nyc1"
-  # Grab the latest version slug from `doctl kubernetes options versions`
-  version = "1.32"
-
+locals {
   tags = [
     "ruby",
     "rails",
     "plant-coach",
-    "dev"
+    "dev",
+    "terraform",
+    "terraform-cloud"
   ]
+  k8s_version = "1.32"
+  application_timezone = "EST"
+  postgresql_version = "17"
+}
+
+resource "digitalocean_project" "plant_coach" {
+  name        = var.project_name
+  description = var.project_description
+  purpose     = "Web Application"
+  environment = var.environment
+  resources = [
+    digitalocean_kubernetes_cluster.plant_coach_cluster.urn,
+    digitalocean_database_cluster.plant_coach_be_cluster.urn
+  ]
+}
+
+resource "digitalocean_kubernetes_cluster" "plant_coach_cluster" {
+  name   = var.app_name
+  region = var.digitalocean_region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = local.k8s_version
+
+  tags = local.tags
 
   node_pool {
-    name       = "worker-pool"
-    size       = "s-2vcpu-2gb"
-    node_count = 1
+    name       = "${var.app_name}-worker-pool"
+    size       = var.node_pool_size
+    node_count = var.node_count
+    tags = local.tags
   }
 }
 
 resource "digitalocean_database_postgresql_config" "plant_coach_be_cluster" {
   cluster_id = digitalocean_database_cluster.plant_coach_be_cluster.id
-  timezone   = "UTC"
-  work_mem   = 16
+  timezone   = local.application_timezone
+}
+
+resource "digitalocean_database_db" "plant-coach-database" {
+  cluster_id = digitalocean_database_cluster.plant_coach_be_cluster.id
+  name       = var.database_name
+}
+
+resource "digitalocean_database_user" "plant-coach-db-user" {
+  cluster_id = digitalocean_database_cluster.plant_coach_be_cluster.id
+  name       = var.backend_database_user_name
 }
 
 resource "digitalocean_database_cluster" "plant_coach_be_cluster" {
-  name       = "plant-coach-be-cluster-postgresql-cluster"
+  name       = var.database_cluster_name
   engine     = "pg"
-  version    = "17"
-  size       = "db-s-1vcpu-1gb"
-  region     = "nyc1"
-  node_count = 1
+  version    = local.postgresql_version
+  size       = var.db_cluster_size
+  region     = var.digitalocean_database_region
+  node_count = var.db_cluster_node_count
+  tags = local.tags
+}
+
+resource "digitalocean_database_connection_pool" "pool-01" {
+  cluster_id = digitalocean_database_cluster.plant_coach_be_cluster.id
+  name       = "pool-01"
+  mode       = "transaction"
+  size       = 5
+  db_name    = var.database_name
+  user       = var.backend_database_user_name
+}
+
+
+resource "digitalocean_database_firewall" "plant_coach_db_firewall" {
+  cluster_id = digitalocean_database_cluster.plant_coach_be_cluster.id
+
+  rule {
+    type  = "k8s"
+    value = digitalocean_kubernetes_cluster.plant_coach_cluster.id
+  }
+
+  rule {
+    type  = "ip_addr"
+    value = var.ip_address_1
+  }
+
+  # rule {
+  #   type  = "droplet"
+  #   value = digitalocean_kubernetes_cluster.plant_coach_cluster.node_pool[0].urn
+  # }
 }
---
+++ vars.tf
@@ -0,0 +1,85 @@
+# The variables represented here are maintainable in this 
+# workspace's Terraform variables for security and reusability.
+# DO NOT change these variable names without also updating
+# the Terraform Cloud workspace variables.
+variable "backend_database_user_name" {
+  type = string
+  default = "backend_database_user_name"
+}
+
+variable "database_name" {
+  type = string
+  default = "database_name"
+}
+
+variable "database_cluster_name" {
+  type = string
+  default = "database_cluster_name"
+}
+
+variable "app_name" {
+  type = string
+  default = "app_name"
+}
+
+variable "project_name" {
+  type = string
+  default = "project_name"
+}
+
+variable "project_description" {
+  type = string
+  default = "Demo Web Application"
+}
+
+# Reference: https://slugs.do-api.dev
+variable "node_pool_size" {
+  type = string
+  default = "s-2vcpu-2gb"
+}
+
+variable "node_count" {
+  type = string
+  default = "1"
+}
+
+# Reference: https://slugs.do-api.dev
+variable "db_cluster_size" {
+  type = string
+  default = "db-s-1vcpu-1gb"
+}
+
+variable "db_cluster_node_count" {
+  type = number
+  default = 1
+}
+
+variable "environment" {
+  type = string
+  default = "development"
+}
+
+variable "ip_address_1" {
+  sensitive = true
+  description = "A user-specified IP address that is whitelisted for DB access."
+  type = string
+  default = "ip_address_1"
+}
+
+variable "digitalocean_database_region" {
+  type = string
+  default = "nyc2"
+}
+
+variable "digitalocean_region" {
+  type = string
+  default = "nyc1"
+}
+
+# This will require more implementation from a module perspective.
+variable "ip_address_2" {
+  sensitive = true
+  description = "A user-specified IP address that is whitelisted for DB access."
+  type = string
+  default = "ip_address_2"
+}
