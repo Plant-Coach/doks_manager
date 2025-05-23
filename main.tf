87a0b5743206f835cff8f38d90e38c1d16168649
Joel Grant <69736499+joel-grant@users.noreply.github.com>
2025-05-23 08:50:36 -0600

    update file names (#12)

--- main.tf
+++ main.tf
@@ -8,19 +8,7 @@ locals {
     "terraform-cloud"
   ]
   k8s_version          = "1.32"
-  application_timezone = "EST"
   postgresql_version   = "17"
-}
-
-resource "digitalocean_project" "plant_coach" {
-  name        = var.project_name
-  description = var.project_description
-  purpose     = "Web Application"
-  environment = var.environment
-  resources = [
-    digitalocean_kubernetes_cluster.plant_coach_cluster.urn,
-    digitalocean_database_cluster.plant_coach_be_cluster.urn
-  ]
 }
 
 resource "digitalocean_kubernetes_cluster" "plant_coach_cluster" {
